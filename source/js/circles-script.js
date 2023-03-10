57b9349dcc8c8f121f1d4cb0c5e0892c28df8ab2
Murad <abishlimurad@gmail.com>
2023-03-11 01:03:16 +0200

    circles script fix

--- source/js/circles-script.js
+++ source/js/circles-script.js
@@ -1,7 +1,8 @@
 const circles = document.querySelectorAll('.services__row-item');
 
 circles.forEach(circle => {
-  circle.addEventListener('click', () => {
-    circle.classList.toggle('active-circle');
+  circle.addEventListener('click', function() {
+    circles.forEach(block => block.classList.remove('active-circle'))
+    this.classList.toggle('active-circle')
   })
 })
